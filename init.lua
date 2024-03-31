e3cb16b2e7382d4d2196c1eb61d75b185337dcaa
Ted <oancea.tudor@icloud.com>
2024-03-31 18:07:25 +0200

    switched to dracula

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "rose-pine",
+  colorscheme = "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
